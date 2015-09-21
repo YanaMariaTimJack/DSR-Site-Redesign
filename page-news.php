1cfa6ef8ebf8f96599010d538380d35345787484
Tim Jack Wilkins <timjackwilkins@gmail.com>
2015-09-21 12:51:38 -0400

    added thumbnails

--- page-news.php
+++ page-news.php
@@ -5,7 +5,14 @@
 */
 
 get_header();  ?>
+	
+    <?php if (has_post_thumbnail() ) {
+  	 the_post_thumbnail('large', array('class' => 'news-hero'));
+ 	} else { ?>
+  	<div class="filler"></div>
+  	<?php	} ?>		
 
+	
 <div class="main">
   <div class="container">
 	
@@ -13,10 +20,21 @@ get_header();  ?>
 		'post_type' => 'news'
 	)); ?>
 
-	<?php if ( $news -> have_posts() ) while ( $news -> have_posts() ) : $news -> the_post(); ?>
+		<?php if ( $news -> have_posts() ) while ( $news -> have_posts() ) : $news -> the_post(); ?>
 
-	  <h2><?php the_title(); ?></h2>
+					
+			<h2><?php the_title(); ?></h2>
+			<span class="date"><?php the_date('d');?></span>	
+			<span class="month-year"><?php the_time('M Y') ?></span>	
+			<!-- could not use the_date() twice on the page, so we used the_time() as a alternative  -->
+
 	  <?php the_content(); ?>
+
+	<?php if (has_post_thumbnail() ) {
+	  the_post_thumbnail();
+	  }	else { ?>
+	  <div class="filler"></div>
+	<?php	} ?>
 
 	<?php endwhile; // end the loop?>
 
