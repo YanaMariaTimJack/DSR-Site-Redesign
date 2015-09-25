44549f465d4a7fd41b00e6492c3015f362df2377
Tim Jack Wilkins <timjackwilkins@gmail.com>
2015-09-25 18:55:08 -0400

    more changes

--- page-news.php
+++ page-news.php
@@ -21,37 +21,41 @@
   		<h1><?php the_title(); ?></h1>
 	</figure>
     			
-<div class="main">
-  <div class="container">
+<div class="container">
 	
 	<?php $news = new WP_Query(array(
 		'post_type' => 'news'
 	)); ?>
 
-		<?php if ( $news -> have_posts() ) while ( $news -> have_posts() ) : $news -> the_post(); ?>
+	<div class="row clearfix">
+		
+			<?php if ( $news -> have_posts() ) while ( $news -> have_posts() ) : $news -> the_post(); ?>
+			<div class="col-xs-4">	
 
-					
-			<h2><?php the_title(); ?></h2>
-			<span class="date"><?php the_date('d');?></span>	
-			<span class="month-year"><?php the_time('M Y') ?></span>	
-			<!-- could not use the_date() twice on the page, so we used the_time() as a alternative  -->
+				<span class="date"><?php the_date('d');?></span>	
+				<span class="month-year"><?php the_time('M Y') ?></span>			
+				<h2><?php the_title(); ?></h2>
+				<!-- could not use the_date() twice on the page, so we used the_time() as a alternative  -->
 
-	  <?php //the_content(); ?>
+			  <?php //the_content(); ?>
 
-	  <!-- This gets the first 30 characters from the content in the post -->
-	  <p><?php echo substr(get_the_excerpt(), 0,30); ?></p>
+			  <!-- This gets the first 30 characters from the content in the post -->
+			  <p><?php echo substr(get_the_excerpt(), 0,30); ?></p>
 
-	<!-- If a features image has been set, this will post that featured image (post_thumbnail). -->
-	<?php if (has_post_thumbnail() ) {
-	  the_post_thumbnail();
-	  }	else { ?>
-	  <!-- However, if a featured image has not been set, a filler div will be put in its place so that the content around doesn't collapse -->
-	  <div class="filler"></div>
-	<?php	} ?>
+			<!-- If a features image has been set, this will post that featured image (post_thumbnail). -->
+			<?php if (has_post_thumbnail() ) {
+			  the_post_thumbnail();
+			  }	else { ?>
+			  <!-- However, if a featured image has not been set, a filler div will be put in its place so that the content around doesn't collapse -->
+			  <div class="filler"></div>
+			<?php	} ?>
+		</div> <!-- end of column -->	
 
-	<?php endwhile; // end the loop?>
+			<?php endwhile; // end the loop?>
+
+	</div>	<!-- end of row -->
 
   </div> <!-- /.container -->
-</div> <!-- /.main -->
+
 
 <?php get_footer(); ?>
