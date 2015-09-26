6925a9b6548f4a8db64450b086dc14b7771c80ad
Yana Tzvetkova <yana@yanatzv.com>
2015-09-26 15:02:16 -0400

    getting the services to work

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,21 @@
 $(function(){
-	//your jQuery here
+	
+  $('.previews a').on('click',function(e){
+    e.preventDefault();
+    console.log('working?');
+    // $(".selected").removeClass("selected");
+    // $(this).addClass("selected");
+    var icon = $(this).data('icon');
+    console.log(icon);
+    var alt = $(this).data('alt');
+    var title = $(this).data('title');
+    console.log(title);
+    var desc = $(this).data('desc');
+    console.log(desc);
+    $('.full img').empty().attr("src", icon);
+    $('.full img').empty().attr("alt", alt);
+    $('.full h3').empty().text(title);
+    $('.full p').empty().text(desc);
+  });
+
 });
