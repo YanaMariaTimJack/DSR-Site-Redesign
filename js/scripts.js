6b9ab76a9105baabe59061741b59f164f488c853
Yana Tzvetkova <yana@yanatzv.com>
2015-09-26 18:01:15 -0400

    services gallery working

--- js/scripts.js
+++ js/scripts.js
@@ -2,18 +2,19 @@ $(function(){
 	
   $('.previews a').on('click',function(e){
     e.preventDefault();
-    console.log('working?');
+    // console.log('working?');
     // $(".selected").removeClass("selected");
     // $(this).addClass("selected");
-    var icon = $(this).data('icon');
-    console.log(icon);
+    var src = $(this).data('src');
+    // console.log(icon);
     var alt = $(this).data('alt');
+    // console.log(alt);
     var title = $(this).data('title');
-    console.log(title);
+    // console.log(title);
     var desc = $(this).data('desc');
-    console.log(desc);
-    $('.full img').empty().attr("src", icon);
-    $('.full img').empty().attr("alt", alt);
+    // console.log(desc);
+    $('.full img').attr("src", src);
+    $('.full img').attr("alt", alt);
     $('.full h3').empty().text(title);
     $('.full p').empty().text(desc);
   });
